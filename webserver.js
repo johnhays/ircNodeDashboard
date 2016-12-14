dba1c05b5be4d02cf80a6cc1d27dd93bef404730
John Hays - K7VE <john@hays.org>
2016-12-13 18:36:20 -0800

    Added STARnet Digital Groups to Reporting

--- webserver.js
+++ webserver.js
@@ -1,4 +1,4 @@
-var express = require('express'), Tail = require('tail').Tail, fs = require('fs'), http = require('http');
+var express = require('express'), Tail = require('tail').Tail, fs = require('fs'), http = require('http'), touch = require('touch');
 var app = express();
 var rfact = new Array(10);
 var linkact = new Array(10);
@@ -6,39 +6,46 @@ var linkptr = 0;
 var rfptr = 0;
 var currentNet = {};
 var currentRF = {};
+var starNets = [];
 var stats = {};
 var dupes = 0;
-var statseconds = 10 * 1000;  // Set number of seconds between updating CPU stats times 1000;
+var config = {};
+var links = [];
+var statseconds = 10 * 1000; // Set number of seconds between updating CPU
+								// stats times 1000;
 
 // Modify paths, if different -- these should work for Compass Linux
 var LinkLOG = '/var/log/opendv/Links.log';
 var ircGW = '/etc/opendv/ircddbgateway';
 var headers = '/var/log/opendv/Headers.log';
+var starNetLog = '/var/log/opendv/STARnet.log';
 
 app.use(express.static(__dirname + '/public'));
 app.set('port', '80');
-app.set('host', ''); // Set to '::' to include IPv6 or set to specific address if not wanted on all interfaces
+app.set('host', '::'); // Set to '::' to include IPv6 or set to specific
+						// address if not wanted on all interfaces
 
 var remove = [ 'ircddbUsername', 'ircddbPassword', 'remotePassword',
 		'remotePort' ];
-var config = {};
-var links = [];
-fs.readFileSync(ircGW).toString().split('\n').forEach(
-		function(line) {
-			var kv = line.split('=', 2);
-			config[kv[0]] = kv[1];
-		});
+fs.readFileSync(ircGW).toString().split('\n').forEach(function(line) {
+	var kv = line.split('=', 2);
+	config[kv[0]] = kv[1];
+});
 
 remove.forEach(function(token) {
 	config[token] = null;
 	delete config[token];
 });
 
-var server = http.createServer(app).listen(app.get('port'), app.get('host'), function() {
-	process.setgid('opendv');
-	process.setuid('opendv');
-	console.log('Express server listening on port ' + app.get('port'));
-});
+var server = http.createServer(app).listen(app.get('port'), app.get('host'),
+		function() {
+			process.setgid('opendv');
+			process.setuid('opendv');
+			touch.sync(LinkLOG, '');
+			touch.sync(headers, '');
+			touch.sync(starNetLog, '');
+			console.log('Express server listening on port ' + app.get('port'));
+		});
 
 var io = require('socket.io')(server);
 
@@ -51,12 +58,10 @@ function trimBetween(str, before, after) {
 	return target;
 }
 
-function orderDS(a,b) {
+function orderDS(a, b) {
 	var ax = Date.parse(a.datestamp);
 	var bx = Date.parse(b.datestamp);
-	// console.log(ax + ' - ' + bx);
 	var res = ax - bx;
-	// console.log(a.datestamp + " - " + b.datestamp + " is " + res);
 	return res;
 }
 
@@ -65,7 +70,6 @@ function addNet(rec) {
 	var r = JSON.stringify(rec);
 	if (c === r) {
 		dupes++;
-		// console.log("Duplicate\n");
 	} else {
 		linkptr %= 10;
 		linkact[linkptr] = rec;
@@ -79,12 +83,46 @@ function addRF(rec) {
 	var r = JSON.stringify(rec);
 	if (c === r) {
 		dupes++;
-		// console.log("Duplicate\n");
 	} else {
 		rfptr %= 10;
 		rfact[rfptr] = rec;
 		rfptr++;
 		currentRF = rec;
+	}
+}
+
+function parseSN(line) {
+	var rest = line.substr(21);
+	var rec = {};
+	rec.datestamp = line.substr(0, 19);
+	rec.date = line.substr(0, 10);
+	rec.time = line.substr(11, 8);
+	var command = rest.substring(0, rest.indexOf(' '));
+	var cursor = rest.indexOf(' ') + 1;
+	switch (command) {
+	case "Adding":
+		rec.callsign = rest.substring(cursor, cursor + 8).trim();
+		cursor = rest.indexOf('group') + 6;
+		rec.group = rest.substring(cursor, cursor + 8).trim();
+		starNets.push(rec);
+		// console.log(command + ' ' + rec.callsign + ' ' + rec.group);
+		break;
+	case "Removing":
+		rec.callsign = rest.substring(cursor, cursor + 8).trim();
+		cursor = rest.indexOf('group') + 6;
+		rec.group = rest.substring(cursor, cursor + 8).trim();
+		for (var i = starNets.length - 1; i > -1; i--) {
+			var o = starNets[i];
+			if (o.callsign == rec.callsign && o.group == rec.group) {
+				starNets.splice(i, 1);
+				// console.log(command + ' ' + rec.callsign + ' ' + rec.group);
+				break;
+			}
+		}
+		break;
+	default:
+		// console.log('STARnet Non-Command: ' + line);
+		break;
 	}
 }
 
@@ -108,13 +146,12 @@ function parseLinks(line) {
 	return rec;
 }
 
-fs.readFileSync(LinkLOG).toString().split('\n').forEach(
-		function(line) {
-			if (line.trim().length > 0) {
-				var linkline = parseLinks(line);
-				links.push(linkline);
-			}
-		});
+fs.readFileSync(LinkLOG).toString().split('\n').forEach(function(line) {
+	if (line.trim().length > 0) {
+		var linkline = parseLinks(line);
+		links.push(linkline);
+	}
+});
 
 function parseLogLine(line) {
 	var rest = line.substr(21);
@@ -137,95 +174,130 @@ function parseLogLine(line) {
 }
 
 var SecondsTohhmmss = function(totalSeconds) {
-        var days    = Math.floor(totalSeconds / 86400);
-        var used    = days * 86400;
-        var hours   = Math.floor((totalSeconds - used) / 3600);
-        used        += hours * 3600;
-        var minutes = Math.floor((totalSeconds - used) / 60);
-        used        += minutes * 60;
-        var seconds = totalSeconds - used;
+	var days = Math.floor(totalSeconds / 86400);
+	var used = days * 86400;
+	var hours = Math.floor((totalSeconds - used) / 3600);
+	used += hours * 3600;
+	var minutes = Math.floor((totalSeconds - used) / 60);
+	used += minutes * 60;
+	var seconds = totalSeconds - used;
 
-        seconds = Math.floor(seconds);
+	seconds = Math.floor(seconds);
 
-        var result = days;
-        result += " " + (hours < 10 ? "0" + hours : hours);
-        result += ":" + (minutes < 10 ? "0" + minutes : minutes);
-        result += ":" + (seconds  < 10 ? "0" + seconds : seconds);
-        return result;
+	var result = days;
+	result += " " + (hours < 10 ? "0" + hours : hours);
+	result += ":" + (minutes < 10 ? "0" + minutes : minutes);
+	result += ":" + (seconds < 10 ? "0" + seconds : seconds);
+	return result;
 }
 
+fs.readFileSync(starNetLog).toString().split('\n').forEach(function(line) {
+	parseSN(line);
+});
 
-io.on('connection', function(socket) {
+io
+		.on(
+				'connection',
+				function(socket) {
 
-	setInterval(function() {
-	        fs.readFileSync("/proc/uptime").toString().split('\n')
-	        .forEach(function(line) {
-	                if (line.trim().length > 0) {
-	                        var timex = line.split(" ");
-				stats['uptime'] = SecondsTohhmmss(timex[0]);
-	                }
-	        });
-	        fs.readFileSync("/proc/loadavg").toString().split('\n')
-	        .forEach(function(line) {
-	                if (line.trim().length > 0) {
-				stats['loadavg'] = line;
-	                }
-	        });
-// This section could be commented out if temp is not available at the indicated file location
-	        fs.readFileSync("/sys/class/thermal/thermal_zone0/temp").toString().split('\n')
-	        .forEach(function(line) {
-	                if (line.trim().length > 0) {
-	                        var temps = line.split(" ");
-	                        var centigrade = temps[0] / 1000;
-	                        var fahrenheit = (centigrade * 1.8) + 32;
-	                        centigrade = Math.round(centigrade * 100) / 100;
-	                        fahrenheit = Math.round(fahrenheit * 100) / 100;
-				stats['cputemp'] = centigrade + "C " + fahrenheit + "F";
-	                }
-	        });
-// end temp section
+					setInterval(
+							function() {
+								fs
+										.readFileSync("/proc/uptime")
+										.toString()
+										.split('\n')
+										.forEach(
+												function(line) {
+													if (line.trim().length > 0) {
+														var timex = line
+																.split(" ");
+														stats['uptime'] = SecondsTohhmmss(timex[0]);
+													}
+												});
+								fs.readFileSync("/proc/loadavg").toString()
+										.split('\n').forEach(function(line) {
+											if (line.trim().length > 0) {
+												stats['loadavg'] = line;
+											}
+										});
+								// This section could be commented out if temp
+								// is not available at the indicated file
+								// location
+								fs
+										.readFileSync(
+												"/sys/class/thermal/thermal_zone0/temp")
+										.toString()
+										.split('\n')
+										.forEach(
+												function(line) {
+													if (line.trim().length > 0) {
+														var temps = line
+																.split(" ");
+														var centigrade = temps[0] / 1000;
+														var fahrenheit = (centigrade * 1.8) + 32;
+														centigrade = Math
+																.round(centigrade * 100) / 100;
+														fahrenheit = Math
+																.round(fahrenheit * 100) / 100;
+														stats['cputemp'] = centigrade
+																+ "C "
+																+ fahrenheit
+																+ "F";
+													}
+												});
+								// end temp section
 
+								socket.emit('stats', stats);
+								// console.log(stats);
+							}, statseconds);
 
-		socket.emit('stats', stats);
-//		console.log(stats);
-	},statseconds);
+					socket.emit('config', config);
+					socket.emit('links', links);
+					socket.emit('starnet', starNets);
 
-
-
-	socket.emit('config', config);
-	socket.emit('links', links);
-
-
-	rfact.sort(orderDS).forEach(function(act) {
-		if (act !== null) {
-			socket.emit('header', act);
-		}
-	});
-	linkact.sort(orderDS).forEach(function(act) {
-		if (act !== null) {
-			socket.emit('header', act);
-		}
-	});
-	rptlog.on('line', function(line) {
-		var lineobj = parseLogLine(line);
-		if (lineobj.source === 'Repeater') {
-			addRF(lineobj);
-		} else {
-			addNet(lineobj);
-		}
-		socket.emit('header', lineobj);
-	});
-	fs.watch(LinkLOG, function(event, filename) {
-		if (event === 'change') {
-			links = [];
-			fs.readFileSync(LinkLOG).toString().split('\n')
-					.forEach(function(line) {
-						if (line.trim().length > 0) {
-							var linkline = parseLinks(line);
-							links.push(linkline);
+					rfact.sort(orderDS).forEach(function(act) {
+						if (act !== null) {
+							socket.emit('header', act);
 						}
 					});
-			socket.emit('links', links);
-		}
-	});
-});
+					linkact.sort(orderDS).forEach(function(act) {
+						if (act !== null) {
+							socket.emit('header', act);
+						}
+					});
+					rptlog.on('line', function(line) {
+						var lineobj = parseLogLine(line);
+						if (lineobj.source === 'Repeater') {
+							addRF(lineobj);
+						} else {
+							addNet(lineobj);
+						}
+						socket.emit('header', lineobj);
+					});
+
+					fs.watch(starNetLog, function(event, filename) {
+						if (event === 'change') {
+							starNets.length = 0;
+							fs.readFileSync(starNetLog).toString().split('\n')
+									.forEach(function(line) {
+										parseSN(line);
+									});
+							socket.emit('starnet', starNets);
+							// console.log(JSON.stringify(starNets));
+						}
+					});
+
+					fs.watch(LinkLOG, function(event, filename) {
+						if (event === 'change') {
+							links = [];
+							fs.readFileSync(LinkLOG).toString().split('\n')
+									.forEach(function(line) {
+										if (line.trim().length > 0) {
+											var linkline = parseLinks(line);
+											links.push(linkline);
+										}
+									});
+							socket.emit('links', links);
+						}
+					});
+				});
